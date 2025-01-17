ff894fd13ac665a32c9bf8b4f3f7b8407f5ebcde
Mathias Bjerke <mathias.bjerke@entur.org>
2025-01-17 08:46:26 +0100

    Added JankyBoarders

--- install.sh
+++ install.sh
@@ -1,5 +1,42 @@
 #!/bin/sh
 
+########################
+### Install software ###
+########################
+
+brew tap FelixKratz/formulae
+brew install borders
+
+
+#####################
+### Link dotfiles ###
+#####################
+
 ln -swf $HOME/.dotfiles/sketchybar $HOME/.config/sketchybar
 ln -swf $HOME/.dotfiles/aerospace $HOME/.config/aerospace
 ln -swf $HOME/.dotfiles/ghostty $HOME/.config/ghostty
+
+
+####################
+### Mac settings ###
+####################
+
+# Turn off "Displays have separate Spaces"
+defaults write com.apple.spaces spans-displays -bool true && killall SystemUIServer
+
+# Turn off Dock delay
+defaults write com.apple.dock autohide-delay -float 0; defaults write com.apple.dock autohide-time-modifier -int 0;killall Dock
+
+# Remap Home and End keys
+mkdir -p $HOME/Library/KeyBindings
+echo '{
+/* Remap Home / End keys to be correct */
+"\UF729" = "moveToBeginningOfLine:"; /* Home */
+"\UF72B" = "moveToEndOfLine:"; /* End */
+"$\UF729" = "moveToBeginningOfLineAndModifySelection:"; /* Shift + Home */
+"$\UF72B" = "moveToEndOfLineAndModifySelection:"; /* Shift + End */
+"^\UF729" = "moveToBeginningOfDocument:"; /* Ctrl + Home */
+"^\UF72B" = "moveToEndOfDocument:"; /* Ctrl + End */
+"$^\UF729" = "moveToBeginningOfDocumentAndModifySelection:"; /* Shift + Ctrl + Home */
+"$^\UF72B" = "moveToEndOfDocumentAndModifySelection:"; /* Shift + Ctrl + End */
+}' > $HOME/Library/KeyBindings/DefaultKeyBinding.dict
