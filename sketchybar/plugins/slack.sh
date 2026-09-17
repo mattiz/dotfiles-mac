50302a3eb3fab281d8241f5ef2bd1c0d7c04a484
Mathias Bjerke <mathias.bjerke@entur.org>
2026-09-17 19:30:20 +0200

    Fix slack unread messages widget

--- sketchybar/plugins/slack.sh
+++ sketchybar/plugins/slack.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-SLACK_INFO=$(lsappinfo info -only StatusLabel `lsappinfo find LSDisplayName=Slack`)
-COUNT=${SLACK_INFO:25:1}
+SLACK_INFO=$(lsappinfo -all info -only StatusLabel Slack 2>/dev/null)
+COUNT=$(echo "$SLACK_INFO" | sed -n 's/.*"label"="\([^"]*\)".*/\1/p')
 
-#echo $SLACK_INFO >> ~/.debug
-#echo $COUNT >> ~/.debug
-
-if [ "$COUNT" = "•" ] || [ "$COUNT" = "\"" ]; then
+if [ -z "$COUNT" ] || [ "$COUNT" = "•" ]; then
   DRAWING=off
 else
   DRAWING=on
