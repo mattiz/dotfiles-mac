bd17252a03967bb03c685ec2df5e04877373d6fd
Mathias Bjerke <mathias.bjerke@entur.org>
2026-09-17 19:34:09 +0200

    feat: add zsh configuration to dotfiles

--- install.sh
+++ install.sh
@@ -16,6 +16,9 @@ ln -swf $HOME/.dotfiles/sketchybar $HOME/.config/sketchybar
 ln -swf $HOME/.dotfiles/aerospace $HOME/.config/aerospace
 ln -swf $HOME/.dotfiles/ghostty $HOME/.config/ghostty
 
+ln -sf $HOME/.dotfiles/zsh/zshrc $HOME/.zshrc
+ln -sf $HOME/.dotfiles/zsh/zprofile $HOME/.zprofile
+
 
 ####################
 ### Mac settings ###
